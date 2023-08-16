a16dc91cda04e2f9c30df1d920a0046ffd20c851
Carolina Jung <83648970+carolina-jung@users.noreply.github.com>
2023-08-16 12:11:46 -0300

    Alterações 16/08 trabalho

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-const robotron = document.querySelector("#robotron");
+const subtrair = document.querySelector("#subtrair");
+const somar = document.querySelector("#somar");
+const braco = document.querySelector("#braco");
 
-robotron.addEventListener("click", dizOi);
-
-function dizOi() {
-    console.log("oi")
-}
+subtrair.addEventListener("click", () => {
+    console.log("Diminui Braço");
+});
